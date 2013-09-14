f25c0f5c0d5d8cda2563fe96d48e797ca67c3d52
Michael Bauer <mihi@lo-res.org>
2013-09-14 07:35:20 -0500

    fully transitioned to jinja fixes #10. Added login and user handling.

--- moodlog.py
+++ moodlog.py
@@ -6,34 +6,66 @@ from google.appengine.ext import ndb
 
 jenv=Environment(loader=FileSystemLoader("templates"))
 
+class Visualisation(webapp2.RequestHandler):
+  
+  def get(self):
+    user=users.get_current_user()
+    if user:
+      t=jenv.get_template("visualisation.html")
+      self.response.headers['Content-type']="text/html ;charset=utf-8"
+      self.response.write(t.render(user=user,logouturl=users.create_logout_url("/")))
+    else: 
+      self.redirect("/")
+
 class MainPage(webapp2.RequestHandler):
   
   def get(self):
-    t=jenv.get_template("visualisation.html")
-    self.response.headers['Content-type']="text/html ;charset=utf-8"
-    self.response.write(t.render())
+    user=users.get_current_user()
+    if user:
+      self.redirect("/log")
+    else:
+      t=jenv.get_template("start.html")
+      self.response.headers['Content-type']="text/html; charset=utf-8"
+      self.response.write(t.render(loginlink=users.create_login_url(self.request.uri)))
 
 class Entries(webapp2.RequestHandler):
   '''getting and creating entries'''  
   def get(self):
-    f=open("data/data_sorted.json")
+    user=users.get_current_user()
     self.response.headers['Content-type']="application/json"
-    self.response.write(f.read())
-    f.close()
+    if user:
+      f=open("data/data_sorted.json")
+      self.response.write(f.read())
+      f.close()
+    else:
+      self.response.write("""{status: "error", reason: "need to log
+      in"}""")
+      
 
   def post(self):
-    note=self.request.get('note')
-    score=self.request.get('score')
+    user=users.get_current_user()
+    self.response.headers['Content-type']="application/json"
+    if user:
+      note=self.request.get('note')
+      score=self.request.get('score')
+    else:
+      self.response.write("""{status: "error", reason: "need to log
+      in"}""")
+      
 
 class Input(webapp2.RequestHandler):
   def get(self):
-    t=jenv.get_template("input.html")
-    self.response.headers['Content-type']="text/html ;charset=utf-8"
-    self.response.write(t.render())
-
+    user=users.get_current_user()
+    if user:
+      t=jenv.get_template("input.html")
+      self.response.headers['Content-type']="text/html ;charset=utf-8"
+      self.response.write(t.render(user=user,logouturl=users.create_logout_url("/")))
+    else:
+      self.redirect("/")
 
 application= webapp2.WSGIApplication([
   ('/',MainPage),
-  ('/input',Input),
+  ('/view',Visualisation),
+  ('/log',Input),
   ('/api/1/entries',Entries),
   ],debug=True)
