1e80997b77c9b8773b24d754fd51853cb2ae7902
Michael Bauer <mihi@lo-res.org>
2013-09-23 19:29:35 +0200

    make ready for move to github and use appengine only as backend...

--- moodlog.py
+++ moodlog.py
@@ -1,101 +1,16 @@
 import webapp2
 import json
 import datetime
-from jinja2 import Environment,FileSystemLoader
 from models import Entry
-from instancecache import InstanceCache
-from google.appengine.api import users
-from google.appengine.ext import ndb
-
-jenv=Environment(loader=FileSystemLoader("templates"))
-cache=InstanceCache()
-
-class Visualisation(webapp2.RequestHandler):
-  
-  def get(self):
-    user=users.get_current_user()
-    if user:
-      self.response.headers['Content-type']="text/html ;charset=utf-8"
-      self.response.headers['Cache-Control']='public, max-age=7200'
-      self.response.headers['Pragma'] = 'Public'
-      p=cache.get("visualisation")
-      if not p:
-        t=jenv.get_template("visualisation.html")
-        self.response.write(cache.set("visualisation",t.render(user=user,logouturl=users.create_logout_url("/"))))
-      else:
-        self.response.write(p)
-    else: 
-      self.redirect("/")
 
 class MainPage(webapp2.RequestHandler):
   
   def get(self):
-    f=open("index.html")
-    self.response.headers['Content-type']="text/html; charset=utf-8"
-    self.response.headers['Cache-Control']='public, max-age=7200'
-    self.response.headers['Pragma'] = 'Public'
-    self.response.write(f.read())
+    self.redirect("http://moodlog.co")
      
-  def head(self):
-      self.response.headers['Content-type']="text/html; charset=utf-8"
-      self.response.headers['Cache-Control']='public, max-age=7200'
-      self.response.headers['Pragma'] = 'Public'
+  def head(self): 
+    self.redirect("http://moodlog.co")
     
-class Entries(webapp2.RequestHandler):
-  '''getting and creating entries'''  
-  def get(self):
-    user=users.get_current_user()
-    self.response.headers['Content-type']="application/json"
-    if user:
-      p=ndb.Key("user-email",user.email())
-      nw=datetime.datetime.now()
-      oy=datetime.datetime(nw.year-1,nw.month+1,1,0,0)
-      entries_query=Entry.query(Entry.time >=oy,ancestor=p)
-      entries=entries_query.fetch()
-      entries=[{"time":e.time.isoformat(), "note":e.note, "score":e.score} for e in
-      entries]
-      self.response.write(json.dumps(entries))
-    else:
-      self.response.write("""{status: "error", reason: "need to log
-      in"}""")
-      
-
-  def post(self):
-    user=users.get_current_user()
-    self.response.headers['Content-type']="application/json"
-    if user:
-      p=ndb.Key("user-email",user.email())
-      entry=Entry(parent=p)
-      entry.author=user;
-      entry.note=self.request.get('note')
-      entry.score=float(self.request.get('score'))
-      entry.put()
-      self.response.write("""{"status": "success" }""")
-    else:
-      self.response.write("""{"status": "error", reason: "need to log
-      in"}""")
-      
-
-class Input(webapp2.RequestHandler):
-  def get(self):
-    user=users.get_current_user()
-    if user:
-      self.response.headers['Content-type']="text/html ;charset=utf-8"
-      self.response.headers['Cache-Control']='public, max-age=7200'
-      self.response.headers['Pragma'] = 'Public'
-      p=cache.get("input")
-      if not p:
-        t=jenv.get_template("input.html")
-        self.response.write(
-          cache.set("input",t.render(user=user,logouturl=users.create_logout_url("/"))))
-      else:
-        self.response.write(p)
-    else:
-      self.redirect("/")
-
 application= webapp2.WSGIApplication([
   ('/',MainPage),
-  ('/view',Visualisation),
-  ('/log',Input),
-  ('/api/1/entries',Entries),
   ],debug=True)
